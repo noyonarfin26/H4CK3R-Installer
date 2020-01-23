87fb50ea29756a465368740f60e744cc501f09b5
noyonarfin26 <57596741+noyonarfin26@users.noreply.github.com>
2020-01-24 03:56:12 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,371 @@
+echo -e "\E[1;33m:::::::::::::::::::::::::::::::::::::: \e[97mH4CK3R IT ZONE \E[1;33m::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -e "\E[1;33m \e[97mLink:https://www.youtube.com/channel/UCcaCMB_QkshPcFpl-PgoNZg/videos?view_as=subscriber \E[1;33m:::::::::::::::::"
+echo -e "\E[1;33m:::::::::::::::::::::::::::::::::::::: \e[97mAUTO INSTALLER\E[1;33m:::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -e "\e[97m                      ______
+                   .-        -.
+                  /            \  By--------Bangladesh Syber Pro-----------
+     \e[94m* \e[97m                   \e[90m* \e[97m
+                 |,  .-.  .-.  ,|        \e[32m* \e[97m
+                 | )(_ /  \_ )( |
+                 |/     /\     \|    \e[34m* \e[97m
+       (@_       <__    ^^    __>         \e[95m* \e[97m
+  _     ) \_______\__|IIIIII|__/____________\e[31m_______________ \e[97m
+ (_)\e[31m@8@8\e[97m{MR.X}<________\e[31m____\e[97m_____________\e[31m__________________/\e[97m
+        )_/         \ IIIIII /                    \e[31m::::::::: \e[97m
+       (@            --------                        \e[31m:: \e[97m
+        "
+tput sgr0                                       # 
+echo -e "\e[31m_________________________[ \e[97m YOU NEED All HACKER TOOL PILLAGE JUT FOR WET
+ INSTALLER  IS RUNING..........................................................................\e[31m]"
+echo "-----------=========Update AND Upgrate Runing=========------------"
+sudo apt-get update && apt-get upgrade
+echo "-----------=========Update AND uprate Complite=========------------"
+echo "-----------=========IDLE Is Runing=========------------"
+
+echo " Y888888P    8888b..       8888        6666BBBBBB "
+echo "    88          8I  dy       //          88       "
+echo "    88          8I  dY       //          66_____  "
+echo "    88          8I  dY       //          88----.  "
+echo "   .88.         8I  dY       //          66       "
+echo " 888888PI     8888Y      i love you jan  jan MR.X+B "
+
+
+sudo apt-get install idle
+echo "-----------=========IDLE Complite=========------------"
+sudo apt-get install figlet
+sudo apt-get install python
+sudo apt-get install python2
+sudo apt-get install python3
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo apt-get install python-pip
+sudo apt install python3-pip
+pip3 install django
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+python get-pip.py --no-index --find-links=/local/copies
+python get-pip.py --user
+python get-pip.py --proxy="http://[user:passwd@]proxy.server:port"
+python get-pip.py pip==9.0.2 wheel==0.30.0 setuptools==28.8.0
+pip install -U pip
+python -m pip install -U pip
+cd
+echo -e $blue"                ..:::::::::.."
+echo -e $blue"           ..:::aad8888888baa:::.."
+echo -e $blue"        .::::d:?88888888888?::8b::::."
+echo -e $blue"      .:::d8888:?88888888??a888888b:::."
+echo -e $blue"    .:::d8888888a8888888aa8888888888b:::."
+echo -e $blue"   ::::dP::::::::88888888888::::::::Yb::::"
+echo -e $blue"  ::::dP:::::::::Y888888888P:::::::::Yb::::"
+echo -e $blue" ::::d8:::::::::::Y8888888P:::::::::::8b::::"
+echo -e $blue".::::88::::::::::::Y88888P::::::::::::88::::."
+echo -e $blue":::::Y8baaaaaaaaaa88P:T:Y88aaaaaaaaaad8P:::::"
+echo -e $blue":::::::Y88888888888P::|::Y88888888888P:::::::"
+echo -e $blue"::::::::::::::::888:::|:::888::::::::::::::::"
+echo -e $blue"::::::::::::::::8888888888888b:::::::::::::::"
+echo -e $blue" :::::::::::::::88888888888888::::::::::::::"
+echo -e $blue"  :::::::::::::888888888888888:::::::::::::"
+echo -e $blue"   ::::::::::::88::88::88:::88::::::::::::"
+echo -e $blue"    :::::::::::88::88::88:::88:::::::::::"
+echo -e $blue"      :::::::::88::88::88:::88:::::::::"
+echo -e $blue"        :::::::88::88:::::::88:::::::"
+echo -e $blue"           :::::::::::::::::::::::"
+echo -e $blue"                :::::::::::::"
+sudo apt-get install tor
+sudo service tor stop
+cd
+
+sudo apt-get install etherape
+sudo apt-get install git
+sudo apt-get install weget
+git clone https://github.com/SimonDevelop/scripts-auto-install.git
+cd scripts-auto-install && ./all-install.sh && bash all-install.sh
+cd
+git clone https://github.com/Und3rf10w/kali-anonsurf.git
+cd kali-anonsurf && bash installer.sh && ./installer.sh
+cd
+
+
+
+
+echo "	                \     /                     "
+echo " 	            \    o ^ o    /                 "
+echo "                \ (     ) /                   "       
+echo " 	     __________(%%%%%%%)____________        "
+echo "	  (     /   /  )%%%%%%%(  \   \     )       "
+echo " 	  (___/___/__/           \__\___\___)       "
+echo " 	    (     /  / (%%%%%%) \  \     )          "
+echo "       (__/___/   (%%%%)  \___\__)            " 
+git clone https://github.com/4w4k3/BeeLogger.git
+cd BeeLogger
+sudo su
+chmod +x install.sh
+./install.sh
+cd
+/usr/local/bin/keylogger
+git clone https://github.com/GiacomoLaw/Keylogger && cd keylogger/mac
+make && make install
+var/log/keystroke.log
+keylogger ~/logfile.txt
+Logging to: /var/log/keystroke.log
+sudo make startup
+cd
+git clone https://github.com/mehulj94/Radium.git
+git clone https://github.com/cyrus-and/xkeylogger.git
+sudo apt-get install libxi-dev
+make
+make static
+xkeylogger > key.log
+cd
+echo "   ___  _    ____ ____ _  _    _  _ _   _ ___  ____ ____ "
+echo "   |__] |    |__| |    |_/     |__|  \_/  |  \ |__/ |__| "
+echo "   |__] |___ |  | |___ | \_    |  |   |   |__/ |  \ |  | "
+echo "   ----------------------------------------------------- "
+git clone https://github.com/Gameye98/Black-Hydra.git
+git clone https://github.com/vanhauser-thc/thc-hydra.git
+cd thc-hydra
+./configure
+make
+make install
+cd
+echo "                                         o \.=./  o       "
+echo "                                           (o o)          "         
+echo "                                       ooO--(_)--Ooo      "
+echo "                                                          "
+echo "                                          O S I F         "
+echo "                             [open source information facebook]"               
+sudo pkg install git python2
+git clone https://github.com/ciku370/OSIF
+
+git clone https://github.com/xHak9x/fbi.git
+cd fbi
+pip2 install -r requirements.txt
+cd
+git clone https://github.com/Steveice10/FBI.git
+git clone https://github.com/fbi-js/fbi.git
+npm i -g fbi
+fbi add https://github.com/fbi-templates/fbi-project-vue.git ..
+cd path/to/workspace
+fbi init vue my-project -o
+cd my-project
+fbi s
+cd
+git clone https://github.com/CarlaAstudillo/ArreStats
+git clone https://github.com/m4ll0k/Infoga
+git clone https://github.com/18F/crime-data-explorer
+git clone https://github.com/glutanimate/PDFMtEd
+git clone https://github.com/tmont/audio-metadata
+git clone https://github.com/amaurymartiny/check-if-email-exists
+git clone https://github.com/Microsoft/app-metadata
+git clone https://github.com/scorelab/ANDROPHSY
+git clone https://github.com/google/turbinia
+git clone https://github.com/D4Vinci/Chrome-Extractor
+git clone https://github.com/unode/firefox_decrypt
+git clone https://github.com/maldevel/IPGeoLocation
+cd IPGeoLocation
+pip3 install -r requirements.txt --user
+apt-get install python3-setuptools
+easy_install3 pip
+pip3 install -r requirements.txt
+
+
+apt install libcurl4-openssl-dev
+export GO111MODULE=on
+go get github.com/Ullaakut/cameradar
+cd $GOPATH/src/github.com/Ullaakut/cameradar
+cd cmd/cameradar
+go install
+cd
+
+echo -e $okegreen""
+echo -e $okegreen"         ____							"
+echo -e $okegreen"        |    | 							"
+echo -e $okegreen"        |____| 							"
+echo -e $okegreen"       _|____|_      $okegreen _____ _       _____     _   _____     _       "
+echo -e $okegreen"        /  $white"ee"\_      $okegreen|_   _| |_ ___|   __|___| |_| __  |___| |_     "
+echo -e $okegreen"      .<     __O     $okegreen  | | |   | -_|   __| .'|  _|    -| .'|  _|	"
+echo -e $okegreen"     /\ \.-.' \      $okegreen  |_| |_|_|___|__|  |___|_| |__|__|___|_|  	"
+echo -e $okegreen"    J  \.|'.\/ \                  				"
+echo -e $okegreen"    | |_.|. | | |   $white"[$okegreen--$white] $cyan " $white"Backdoor" Creator for Remote Acces $white[$okegreen--$white] 	"
+echo -e $okegreen"     \__.' .|-' /   $white"[$okegreen--$white] $cyan"           H4CK3R IT ZONE             $white[$okegreen--$white]	"
+echo -e $okegreen"     L   /|o'--'\   $white"[$okegreen--$white] $cyan"https://www.youtube.com/channel/UCcaCMB_QkshPcFpl-PgoNZg/videos?view_as=subscriber $white[$okegreen--$white]	"
+echo -e $okegreen"     |  /\/\/\   \  $white"[$okegreen--$white] $cyan"---------------------------------------$white[$okegreen--$white]       "
+echo -e $okegreen"     J /      \.__\ $white"[$okegreen--$white] $cyan"          AUTO INSTALLER               $white[$okegreen--$white]       "
+echo -e $okegreen"     J /      \.__\ $white"[$okegreen--$white] $cyan"---------------------------------------$white[$okegreen--$white]       "
+echo -e $okegreen"     |/         /   $white"[$okegreen--$white] $cyan"-------------MR.X----------------------$white[$okegreen--$white]	"
+echo -e $okegreen"       \      .'\.  $white"[$okegreen--$white] $white"-----Bangladesh Syber Pro-------------$white[$okegreen--$white]       "
+echo -e $okegreen"    ____)_/\_(___\. $white"[$okegreen--$white] ".___________________________________$white[$okegreen--$white]	        "
+echo -e $okegreen"   (___._/  \_.___)'$white\_.-----------------------------------------/"
+echo -e "\e[31m_________________________[ \e[97m YOU NEED All HACKER TOOL PILLAGE JUT FOR WET
+ INSTALLER  IS RUNING..........................................................................\e[31m]"
+git clone https://github.com/Exploit-install/TheFatRat.git
+cd TehFatrat/Setup
+chmod +x setup.sh && ./setup.sh
+cd
+echo "==========================================================================="
+echo "                                                                           "
+echo "888     888 8888888888 .d88888b.  888b    888          888                 "
+echo "888     888 888        d88PY888b  8888b   888          888                 " 
+echo "888     888 888       888     888 88888b  888          888                 " 
+echo "888     888 8888888   888     888 888Y88b 888  .d88b.  888888              " 
+echo "888     888 888       888     888 888 Y88b888 d8P  Y8b 888                 "  
+echo "888     888 888       888     888 888  Y88888 88888888 888                 "    
+echo "Y88b. .d88P 888       Y88b. .d88P 888   Y8888 Y8b.     Y88b.               "  
+echo "  Y88888P  888          Y88888P   888    Y888  Y8888   Y8888             "
+echo "                                                                           "
+echo "===========================================================================" 
+git clone https://github.com/epsylon/ufonet.git
+sudo python setup.py install
+sudo pip install geoip 
+sudo pip install requests
+sudo pip install pycrypto
+echo  "           _____  |__|______  ____   ____   __| _/__| _/____   ____"
+echo  "           \__  \ |  \_  __ \/ ___\_/ __ \ / __ |/ __ |/  _ \ /    \\"
+echo  "            / __ \|  ||  | \/ /_/  >  ___// /_/ / /_/ (  <_> )   |  \\"
+echo  "           (____  /__||__|  \___  / \___  >____ \____ |\____/|___|  /"
+echo  "                \/         /_____/      \/     \/    \/           \/"
+echo "                         .   *       _.---._  *              "
+echo "                                   .'       '.       .       "
+echo "                               _.-~===========~-._        *  "
+echo "                           *  (___________________)     .    "
+echo "                       .     .      \_______/    *           "
+echo "                                                             "
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon.git
+cd airgeddon && chmod +x airgeddon.sh
+cd
+git clone https://github.com/ihebski/angryFuzzer.git
+git clone https://github.com/govolution/avet.git
+cd avet
+./setup.sh
+cd
+git clone https://github.com/engMaher/BAF.git
+git clone https://github.com/beefproject/beef.git
+cd beef
+./install
+cd
+git clone https://github.com/Gameye98/Auxscan.git
+
+echo  "         ██╗      █████╗ ███████╗██╗   ██╗"
+echo  "         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝"
+echo  "         ██║     ███████║  ███╔╝  ╚████╔╝ "
+echo  "         ██║     ██╔══██║ ███╔╝    ╚██╔╝  "
+echo  "    The  ███████╗██║  ██║███████╗   ██║   "
+echo  "         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   "
+cd
+git clone https://github.com/arismelachroinos/lscript.git
+cd lscript
+chmod +x install.sh
+./install.sh
+cd
+git clone https://www.github.com/MetaChar/Mercury
+cd Mercury
+chmod +x requirements.sh
+cd
+git clone https://github.com/Mebus/cupp.git
+git clone https://github.com/n1nj4sec/pupy.git
+git clone https://github.com/thelinuxchoice/ddostor.git
+git clone https://github.com/grafov/hulk.git
+git clone https://github.com/gkbrk/slowloris.git
+cd slowloris
+python3 slowloris.py -h
+cd
+https://github.com/Ha3MrX/DDos-Attack.git
+cd DDos-Attack
+chmod +x ddos-attack.py
+python ddos-attack.py
+cd
+git clone https://github.com/649/Memcrashed-DDoS-Exploit.git
+cd Memcrashed-DDoS-Exploit
+echo "SHODAN_KEY" > api.txt
+docker build -t memcrashed .
+docker run -it memcrashed
+pip install scapy
+pip install shodan
+echo "
+███████╗███████╗ ██████╗  ██████╗██╗███████╗████████╗██╗   ██╗
+██╔════╝██╔════╝██╔═══██╗██╔════╝██║██╔════╝╚══██╔══╝╚██╗ ██╔╝
+█████╗  ███████╗██║   ██║██║     ██║█████╗     ██║    ╚████╔╝
+██╔══╝  ╚════██║██║   ██║██║     ██║██╔══╝     ██║     ╚██╔╝
+██║     ███████║╚██████╔╝╚██████╗██║███████╗   ██║      ██║
+╚═╝     ╚══════╝ ╚═════╝  ╚═════╝╚═╝╚══════╝   ╚═╝      ╚═╝ "
+git clone https://github.com/Manisso/fsociety.git
+cd fsociety && bash <(wget -qO- https://git.io/vAtmB)
+cd
+git clone https://www.github.com/threat9/routersploit
+cd routersploit
+sudo python3 -m pip install -r requirements.txt
+cd
+git clone https://github.com/linkedin/qark
+cd qark
+pip install -r requirements.txt
+pip install . 
+cd
+git clone https://github.com/xHak9x/SocialPhish.git
+cd SocialPhish
+chmod +x socialphish.sh
+cd
+git clone https://github.com/GitHackTools/Leaked
+cd Leaked
+./install_update.bat
+cd 
+git clone https://github.com/s0md3v/Striker.git
+git clone https://github.com/ClusterLabs/striker.git
+git clone https://github.com/markusfisch/Striker.git
+git clone https://github.com/sammbertram/Striker.git
+git clone https://github.com/intraspeciespredator161/Striker.git
+cd Striker
+pip install -r requirements.txt
+cd
+git hub https://github.com/StackExchange/blackbox.git
+
+echo "---------------Installation Instructions------------------
+
+    The hard way (manual): Copy all the files in "bin" to your "bin".
+    The hard way (automatic): make copy-install will copy the bin files into /usr/local/bin (uninstall with make usrlocal-uninstall).
+    The symlinks way: make symlinks-install will make symlinks of the bin files into /usr/local/bin (uninstall with make usrlocal-uninstall) (useful when doing development)
+    The MacPorts Way: sudo port install vcs_blackbox
+    The Homebrew Way: brew install blackbox
+    The RPM way: Check out the repo and make an RPM via make packages-rpm; now you can distribute the RPM via local methods. (Requires fpm.)
+    The Debian/Ubuntu way: Check out the repo and make a DEB via make packages-deb; now you can distribute the DEB via local methods. (Requires fpm.)
+    The Antigen Way: Add antigen bundle StackExchange/blackbox to your .zshrc
+    The Zgen Way: Add zgen load StackExchange/blackbox to your .zshrc where you're loading your other plugins.
+    The Nix Way: nix-env -i blackbox
+--------------------------------------------------------------------"
+git clone https://github.com/jaygreig86/dmitry.git
+cd dmitry
+man dmitry
+./dmitry
+cd
+git clone https://github.com/MrHolako/Install-Metasploit-Auto.git
+cd Install-Metasploit-Auto
+ls
+chmod +x Install-Metasploit.sh && bash Install-Metasploit.sh
+cd
+git clone https://github.com/trustedsec/social-engineer-toolkit/ set/
+cd set
+sudo pip install -r requirements.txt
+cd
+git clone https://github.com/savio-code/fern-wifi-cracker.git
+git clone https://github.com/derv82/wifite.git
+echo -e "\E[1;33m:::::::::::::::::::::::::::::::::::::: \e[97mH4CK3R IT ZONE \E[1;33m::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -e "\E[1;33m \e[97mLink:https://www.youtube.com/channel/UCcaCMB_QkshPcFpl-PgoNZg/videos?view_as=subscriber \E[1;33m:::::::::::::::::"
+echo -e "\E[1;33m:::::::::::::::::::::::::::::::::::::: \e[97mAUTO INSTALLER\E[1;33m:::::::::::::::::::::::::::::::::::::::::::::::::::::"
+echo -e "\e[97m                      ______
+                   .-        -.
+                  /            \  By--------Bangladesh Syber Pro-----------
+     \e[94m* \e[97m                   \e[90m* \e[97m
+                 |,  .-.  .-.  ,|        \e[32m* \e[97m
+                 | )(_ /  \_ )( |
+                 |/     /\     \|    \e[34m* \e[97m
+       (@_       <__    ^^    __>         \e[95m* \e[97m
+  _     ) \_______\__|IIIIII|__/____________\e[31m_______________ \e[97m
+ (_)\e[31m@8@8\e[97m{MR.X}<________\e[31m____\e[97m_____________\e[31m__________________/\e[97m
+        )_/         \ IIIIII /                    \e[31m::::::::: \e[97m
+       (@            --------                        \e[31m:: \e[97m
+        "
+tput sgr0                                       # 
+echo -e "\e[31m_________________________[ \e[97m YOU NEED All HACKER TOOL PILLAGE JUT FOR WET
+echo "-----------=========Complite=========------------"
+done
+
+
